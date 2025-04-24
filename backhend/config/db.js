e3fe3d42a4b2bfb0887e7f78550202097a912d03
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-24 13:30:32 +0100

    Create db.js

---
+++ backhend/config/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("✅ Database Connected Successfully"))
+    .catch(err => console.error("❌ MongoDB Connection Failed", err));
+
+module.exports = mongoose
