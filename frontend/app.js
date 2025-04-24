043b7dc12f29455370da7b546d4bbe64a644ea85
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-24 14:25:29 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Settings from "./pages/Settings";
+import API_Docs from "./pages/API_Docs";
+import AdminDashboard from "./pages/AdminDashboard";
 
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/settings" element={<Settings />} />
+                <Route path="/api-docs" element={<API_Docs />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
